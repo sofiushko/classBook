3b71c677fa098bde2e9c5f6942a076885e698957
Sonja <sofiushko@gmail.com>
2013-09-30 21:45:24 +0400

    Init index.html with requirejs and backbone

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+﻿require.config({
+
+    paths: {
+            'jquery': 'vendor/jquery',
+            'underscore': 'vendor/underscore',
+            'backbone': 'vendor/backbone',
+    }, 
+
+    shim: {
+         'jquery': { exports: '$' },
+         'underscore': { exports: '_' },
+         'backbone': { exports: 'Backbone', deps: ['jquery', 'underscore'] }
+    }
+});
